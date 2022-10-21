bee4409e065bb3a4082b5c2e42a07554878bb882
18Jers <jersson15leon@gmail.com>
2022-10-21 12:22:58 -0500

    Chat-Java :D

---
+++ spring-tool-websocket/src/main/java/com/spring/chat/SpringToolWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.spring.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToolWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringToolWebsocketApplication.class, args);
+	}
+
+}
